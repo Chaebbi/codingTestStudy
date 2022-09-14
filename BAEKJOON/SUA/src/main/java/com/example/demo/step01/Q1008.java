8072ca5f41508af3aa2dbbaf8ad66cc9ad3560af
rosesua318 <rosesua@duksung.ac.kr>
2022-09-15 00:12:39 +0900

    [BAEKJOON-STEP01-01] 입출력과 사칙연산 단계 8번까지 (김민서)

---
+++ BAEKJOON/SUA/src/main/java/com/example/demo/step01/Q1008.java
@@ -0,0 +1,16 @@
+package com.example.demo.step01;
+
+import java.util.Scanner;
+
+public class Q1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        System.out.println(a / b);
+
+        sc.close();
+    }
+}
