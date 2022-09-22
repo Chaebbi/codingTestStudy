0c015a93001d91aaa4ef55bc0bccf6b5d02f65b4
rosesua318 <rosesua@duksung.ac.kr>
2022-09-23 00:02:35 +0900

    [INFLEARN-SECTION08-SUA] DFS, BFS 활용 수열 추측하기까지 (낌민서)

---
+++ INFLEARN/SUA/src/main/java/com/example/demo/SECTION08/Q02.java
@@ -0,0 +1,31 @@
+package com.example.demo.SECTION08;
+
+import java.util.*;
+
+public class Q02 {
+    static int answer = Integer.MIN_VALUE, c, n;
+
+    public void DFS(int L, int sum, int[] arr){
+        if(sum > c) return;
+        if(L == n){
+            answer = Math.max(answer, sum);
+        }
+        else{
+            DFS(L + 1, sum + arr[L], arr);
+            DFS(L + 1, sum, arr);
+        }
+    }
+
+    public static void main(String[] args){
+        Q02 T = new Q02();
+        Scanner kb = new Scanner(System.in);
+        c = kb.nextInt();
+        n = kb.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = kb.nextInt();
+        }
+        T.DFS(0, 0, arr);
+        System.out.println(answer);
+    }
+}
