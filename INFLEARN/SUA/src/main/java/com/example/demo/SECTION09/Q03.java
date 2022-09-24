e2213efb53b78c5bebbb952117b8166b2604ba18
rosesua318 <rosesua@duksung.ac.kr>
2022-09-25 00:03:13 +0900

    [INFLEARN-SECTION09-SUA] Greedy Algorithm 최대수입스케쥴까지 (김민서)

---
+++ INFLEARN/SUA/src/main/java/com/example/demo/SECTION09/Q03.java
@@ -0,0 +1,52 @@
+package com.example.demo.SECTION09;
+
+import java.util.*;
+
+class Time2 implements Comparable<Time2> {
+    public int time;
+    public char state;
+    Time2(int time, char state) {
+        this.time = time;
+        this.state = state;
+    }
+    @Override
+    public int compareTo(Time2 ob) {
+        if(this.time == ob.time) {
+            return this.state - ob.state;
+        } else {
+            return this.time - ob.time;
+        }
+    }
+}
+
+public class Q03 {
+    public int solution(ArrayList<Time2> arr) {
+        int answer = Integer.MIN_VALUE;
+        Collections.sort(arr);
+        int cnt = 0;
+        for(Time2 ob : arr) {
+            if(ob.state == 's') {
+                cnt++;
+            }
+            else {
+                cnt--;
+            }
+            answer = Math.max(answer, cnt);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Q03 T = new Q03();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        ArrayList<Time2> arr = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            int sT = kb.nextInt();
+            int eT = kb.nextInt();
+            arr.add(new Time2(sT, 's'));
+            arr.add(new Time2(eT, 'e'));
+        }
+        System.out.println(T.solution(arr));
+    }
+}
