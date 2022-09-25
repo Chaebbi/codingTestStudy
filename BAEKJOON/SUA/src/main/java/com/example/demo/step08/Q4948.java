86b125f50effbf0bc926267112eddb0f6ae07586
rosesua318 <rosesua@duksung.ac.kr>
2022-09-25 23:50:28 +0900

    [BAEKJOON-STEP08-SUA] 기본수학2 완료 (김민서)

---
+++ BAEKJOON/SUA/src/main/java/com/example/demo/step08/Q4948.java
@@ -0,0 +1,37 @@
+package com.example.demo.step08;
+
+import java.util.Scanner;
+
+public class Q4948 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            int n = sc.nextInt();
+
+            if (n == 0) {
+                break;
+            }
+
+            boolean arr[] = new boolean[2 * n + 1];
+            arr[0] = true;
+            arr[1] = true;
+
+            for (int i = 2; i <= Math.sqrt(2 * n + 1); i++) {
+                for (int j = i * i; j < 2 * n + 1; j += i) {
+                    arr[j] = true;
+                }
+            }
+
+            int cnt = 0;
+            for (int i = n + 1; i < 2 * n + 1; i++) {
+                if (arr[i] == false) {
+                    cnt++;
+                }
+            }
+            System.out.println(cnt);
+        }
+
+        sc.close();
+    }
+}
