24586ac917c96fb3ec57d1ac9019683dbf8b584f
Vector1331 <tpgus4796@gmail.com>
2022-09-29 22:56:35 +0900

    [INFLEARN-SLIDING-03] Sliding window

--- INFLEARN/VECTOR/codingTest/src/main/java/sec03/Main3.java
+++ INFLEARN/VECTOR/codingTest/src/main/java/sec03/Main3.java
@@ -2,25 +2,29 @@ package sec03;
 
 import java.util.*;
 
-// 최대 매출 : n일간 매출기록, 연속된 k일간 최대 매출액을 구하려 함
+// [슬라이딩윈도우] 최대 매출 : n일간 매출기록, 연속된 k일간 최대 매출액을 구하려 함
 public class Main3 {
-    private ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
-        ArrayList<Integer> ans = new ArrayList<>();
+    private int solution(int n, int k, int[] arr) {
+        int ans = 0, sum = 0;
+        for(int i = 0; i < k; i++){
+            sum += arr[i];
+        }
+        ans = sum;
+        for(int i = k; i < n; i++){
+            sum += (arr[i] - arr[i-k]);
+            ans = Math.max(ans, sum);
+        }
         return ans;
     }
     public static void main(String[] args) {
         Main3 T = new Main3();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-        int[] a = new int[n];
+        int k = kb.nextInt();
+        int[] arr = new int[n];
         for(int i=0; i<n; i++){
-            a[i] = kb.nextInt();
+            arr[i] = kb.nextInt();
         }
-        int m = kb.nextInt();
-        int[] b = new int[m];
-        for(int i=0; i<m; i++){
-            b[i] = kb.nextInt();
-        }
-        for(int x : T.solution(n, m, a, b)) System.out.print(x + " ");
+        System.out.print(T.solution(n, k, arr));
     }
 }
