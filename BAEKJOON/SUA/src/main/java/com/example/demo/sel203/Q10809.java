e41694c514783a54b29120b9c3ff227a09f083b2
rosesua318 <rosesua@duksung.ac.kr>
2022-10-08 00:31:14 +0900

    [BAEKJOON-SEL203-SUA] 자료구조1 (참고) 문자열 분석까지 (김민서)

---
+++ BAEKJOON/SUA/src/main/java/com/example/demo/sel203/Q10809.java
@@ -0,0 +1,30 @@
+package com.example.demo.sel203;
+
+import java.util.Scanner;
+
+public class Q10809 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[26];
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = -1;
+        }
+
+        String str = sc.next();
+
+        for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if(arr[c - 'a'] == -1) {
+                arr[c - 'a'] = i;
+            }
+        }
+
+        for(int a : arr) {
+            System.out.print(a + " ");
+        }
+
+        sc.close();
+    }
+}
