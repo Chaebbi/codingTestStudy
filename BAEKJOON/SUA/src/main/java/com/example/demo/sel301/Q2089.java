adc7fd2f7cae7adc9cad21158de722f9dfc67565
rosesua318 <rosesua@duksung.ac.kr>
2022-10-13 00:18:34 +0900

    [BAEKJOON-SEL301-SUA] 수학1 (연습) 끝까지 완료 (김민서)

---
+++ BAEKJOON/SUA/src/main/java/com/example/demo/sel301/Q2089.java
@@ -0,0 +1,25 @@
+package com.example.demo.sel301;
+
+import java.util.Scanner;
+
+public class Q2089 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int n = sc.nextInt();
+
+        if(n == 0) {
+            System.out.println(0);
+        } else {
+            while(n != 1) {
+                sb.append(Math.abs(n % -2));
+                n = (int)Math.ceil((double)n / (-2));
+            }
+            sb.append(n);
+        }
+        System.out.println(sb.reverse());
+
+        sc.close();
+    }
+}
